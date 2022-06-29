9d565d5740039b53acdf7320ff1cd45183386c6e
Zachary Traczyk <cookies951@gmail.com>
2022-06-29 15:04:56 -0500

    basic component and styling structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
+import Form from './components/form/Form';
+import Graph from './components/graphs/Graph';
+
+import './styles/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Sidebar />
+      <div className="main-container">
+       <Form />
+       <Graph />
+      </div>
     </div>
   );
 }
