9701e2b8db0ceb247deef99b4806f1b14eb24da1
Zachary Traczyk <cookies951@gmail.com>
2022-06-29 15:41:52 -0500

    Basic Form functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,11 +6,14 @@ import Graph from "./components/graphs/Graph";
 import "./styles/App.scss";
 
 const App = () => {
+  const logData = (data: Object) => {
+    console.log("App.tsx: form data submitted:",data);
+  }
   return (
     <div className="app">
       <Sidebar />
       <div className="main-container">
-        <Form />
+        <Form submitData={(data: Object) => logData(data)}/>
         <Graph />
       </div>
     </div>
