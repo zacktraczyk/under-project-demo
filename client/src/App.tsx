e11be5ca895dc4c3f4c3676e43b5b55ccb85046d
Zachary Traczyk <cookies951@gmail.com>
2022-06-29 15:10:59 -0500

    change react components to functional

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import Sidebar from './components/sidebar/Sidebar';
-import Form from './components/form/Form';
-import Graph from './components/graphs/Graph';
+import React from "react";
+import Sidebar from "./components/sidebar/Sidebar";
+import Form from "./components/form/Form";
+import Graph from "./components/graphs/Graph";
 
-import './styles/App.scss';
+import "./styles/App.scss";
 
-function App() {
+const App = () => {
   return (
     <div className="app">
       <Sidebar />
       <div className="main-container">
-       <Form />
-       <Graph />
+        <Form />
+        <Graph />
       </div>
     </div>
   );
-}
+};
 
 export default App;
