fce22beb9640c31908a1cfad7736b1254e5000f2
Ben-Ferreira <benjamin@ferreira-Family.com>
2022-07-01 12:40:52 -0500

    part way through login page styling

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -4,6 +4,7 @@ import UserPool from "./UserPool";
 import LoginUser from "./LoginUser";
 import CreateUser from "./CreateUser";
 import "../../styles/components/Login.scss";
+import exampleDesktop from "../../assets/desktop.jpg";
 
 interface LoginProps {
   onSubmission: Function;
@@ -16,67 +17,74 @@ export default function Login(props: LoginProps) {
   console.log(loginError);
 
   return (
-    <div className="login">
-      <motion.div
-        initial={{ y: 100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        exit={{ y: -100, opacity: 0 }}
-      >
-        <ol>
-          <li>
-            <h1
-              className={loginSlide ? "selected" : ""}
-              onClick={() => {
-                setLoginSlide(true);
-                setLoginError(null);
-              }}
-            >
-              Login
-            </h1>
-          </li>
-          <li>
-            <h1
-              className={!loginSlide ? "selected" : ""}
-              onClick={() => {
-                setLoginSlide(false);
-                setLoginError(null);
-              }}
-            >
-              Create User
-            </h1>
-          </li>
-        </ol>
+    <div className="login-page-container">
+      <div className="login">
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -100, opacity: 0 }}
+        >
+          <ol>
+            <li>
+              <h1
+                className={loginSlide ? "selected" : ""}
+                onClick={() => {
+                  setLoginSlide(true);
+                  setLoginError(null);
+                }}
+              >
+                Login
+              </h1>
+            </li>
+            <li>
+              <h1
+                className={!loginSlide ? "selected" : ""}
+                onClick={() => {
+                  setLoginSlide(false);
+                  setLoginError(null);
+                }}
+              >
+                Create User
+              </h1>
+            </li>
+          </ol>
 
-        <div className="slider-container">
-          <AnimatePresence>
-            {loginSlide && (
-              <LoginUser
-                onLogin={props.onSubmission}
-                displayError={(error: Error) => setLoginError(error)}
-              />
-            )}
-            {!loginSlide && (
-              <CreateUser
-                onLogin={props.onSubmission}
-                displayError={(error: Error) => setLoginError(error)}
-                setLoginSlide={setLoginSlide}
-              />
-            )}
+          <div className="slider-container">
+            <AnimatePresence>
+              {loginSlide && (
+                <LoginUser
+                  onLogin={props.onSubmission}
+                  displayError={(error: Error) => setLoginError(error)}
+                />
+              )}
+              {!loginSlide && (
+                <CreateUser
+                  onLogin={props.onSubmission}
+                  displayError={(error: Error) => setLoginError(error)}
+                  setLoginSlide={setLoginSlide}
+                />
+              )}
 
-            {loginError != undefined && (
-              <div className="error-container">
-                <motion.div
-                  initial={{ y: -30, opacity: 0 }}
-                  animate={{ y: 0, opacity: 1 }}
-                  exit={{ y: 30, opacity: 0 }}
-                >
-                  <p className="request-error">{"" + loginError.message}</p>
-                </motion.div>
-              </div>
-            )}
-          </AnimatePresence>
-        </div>
-      </motion.div>
+              {loginError != undefined && (
+                <div className="error-container">
+                  <motion.div
+                    initial={{ y: -30, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    exit={{ y: 30, opacity: 0 }}
+                  >
+                    <p className="request-error">{"" + loginError.message}</p>
+                  </motion.div>
+                </div>
+              )}
+            </AnimatePresence>
+          </div>
+        </motion.div>
+      </div>
+      <div className="welcome-image-container">
+        <h2>Nice to see you again</h2>
+        <h1>Welcome Back</h1>
+        <img src={exampleDesktop} className="welcome-image"></img>
+      </div>
     </div>
   );
 }
